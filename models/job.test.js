440d5b45db9eb816632eb08d1d9361ddeb15dc46
jeffng5 <jeffreykng@hotmail.com>
2024-01-20 17:04:14 -0500

    wrote tests

--- models/job.test.js
+++ models/job.test.js
@@ -1,32 +1,116 @@
-const Job = require("./job.js");
+"use strict";
+
 const db = require("../db.js");
+const { BadRequestError, NotFoundError } = require("../expressError");
+const Company = require("./company.js");
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+} = require("./_testCommon");
 
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
 
+/************************************** create */
 
+describe("create", function () {
+  const newJob = {
+    title: 'software engineer',
+    salary: 50000,
+    equity: 0.75,
+    company_handle: 'microsoft',
+    
+  };
 
-async function beforeAll() {
-const result = await db.query(`INSERT INTO jobs (title, salary, equity, company_handle)
-VALUES ('c1', 20000, .5, "C1"`)}
-beforeAll()
+  test("works", async function () {
+    let job = await Company.create(newJob);
+    expect(job).toEqual(newJob);
+
+    const result = await db.query(
+          `SELECT title, salary, equity, company_handle
+           FROM jobs
+           WHERE company_handle = 'microsoft'`);
+    expect(result.rows).toEqual([
+      {
+        title: "software engineer",
+        salary: 50000,
+        equity: 0.75,
+        company_handle: 'microsoft',
+      },
+    ]);
+  });
+
+  test("bad request with dupe", async function () {
+    try {
+      await Job.create(newJob);
+      await Job.create(newJob);
+      fail();
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
+});
+
+/************************************** findAll */
 
 describe("findAll", function () {
-    test("finds all jobs", async function() {
+  test("works: no filter", async function () {
+    let jobs = await Job.findAll();
+    expect(companies).toEqual([
+      {
+        title: "software engineer",
+        salary: 50000,
+        equity: 0.75,
+        company_handle: "microsoft"
+      },
+   
+    ]);
+  });
+});
 
+/************************************** get */
 
-        let jobs = await Job.findAll();
-        expect(jobs).toEqual([
-            {
-               title: "c1",
-               salary: 20000,
-               equity: .5,
-               company_handle: "C1" 
-            }
+describe("get", function () {
+  test("works", async function () {
+    let job = await Job.get("microsoft");
+    expect(job).toEqual({
+      title: "software engineer",
+      salary: 50000,
+      equity: .075,
+      company_handle: 'microsoft'
+    });
+  });
 
-        ])
+  test("not found if no such job", async function () {
+    try {
+      await Job.get("nope");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
     }
-    
-    )
+  });
+});
 
+/************************************** remove */
 
+describe("remove", function () {
+  test("works", async function () {
+    await Job.remove("microsoft");
+    const res = await db.query(
+        "SELECT company_handle FROM jobs WHERE company_handle='microsoft'");
+    expect(res.rows.length).toEqual(0);
+  });
 
-})
+  test("not found if no such job", async function () {
+    try {
+      await Job.remove("nope");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
---
+++ routes/jobs.test.js
@@ -0,0 +1,163 @@
+"use strict";
+
+const request = require("supertest");
+
+const db = require("../db");
+const app = require("../app");
+
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+  u1Token,
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** POST /jobs */
+
+describe("POST /jobs", function () {
+  const newJob = {
+    title: "software engineer",
+    salary: 50000,
+    equity: 0.75,
+    company_handle: "microsoft",
+    
+  };
+
+  test("ok for users", async function () {
+    const resp = await request(app)
+        .post("/jobs")
+        .send(newJob)
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(201);
+    expect(resp.body).toEqual({
+      job: newJob,
+    });
+  });
+
+  test("bad request with missing data", async function () {
+    const resp = await request(app)
+        .post("/jobs")
+        .send({
+          equity: 0.75,
+          company_handle: "microsoft",
+        })
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("bad request with invalid data", async function () {
+    const resp = await request(app)
+        .post("/jobs")
+        .send({
+          ...newJob,
+          logoUrl: "not-a-url",
+        })
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(400);
+  });
+});
+
+/************************************** GET /jobs */
+
+describe("GET /jobs", function () {
+  test("ok for anon", async function () {
+    const resp = await request(app).get("/jobs");
+    expect(resp.query).toEqual({
+      jobs:[
+          {
+            title : "software engineer"
+          }
+      ]
+    })
+    expect(resp.body).toEqual({
+      companies:
+          [
+            {
+              title: 'software engineer',
+              salary: 50000,
+              equity: 0.75,
+              company_handle: 'microsoft',
+              logoUrl: "http://c1.img",
+            },
+          
+          ],
+    });
+  });
+
+  test("fails: test next() handler", async function () {
+    // there's no normal failure event which will cause this route to fail ---
+    // thus making it hard to test that the error-handler works with it. This
+    // should cause an error, all right :)
+    await db.query("DROP TABLE companies CASCADE");
+    const resp = await request(app)
+        .get("/companies")
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(500);
+  });
+});
+
+/************************************** GET /companies/:handle */
+
+describe("GET /jobs/:handle", function () {
+  test("works for anon", async function () {
+    const resp = await request(app).get(`/companies/c1`);
+    expect(resp.body).toEqual({
+      job: {
+        title: "software engineer",
+        salary: 50000,
+        equity: 0.75,
+        company_handle: "microsoft",
+        logoUrl: "http://c1.img",
+      },
+    });
+  });
+
+//   test("works for anon: company w/o jobs", async function () {
+//     const resp = await request(app).get(`/companies/c2`);
+//     expect(resp.body).toEqual({
+//       company: {
+//         handle: "c2",
+//         name: "C2",
+//         description: "Desc2",
+//         numEmployees: 2,
+//         logoUrl: "http://c2.img",
+//       },
+//     });
+//   });
+
+  test("not found for no such company", async function () {
+    const resp = await request(app).get(`/companies/nope`);
+    expect(resp.statusCode).toEqual(404);
+  });
+});
+
+
+/************************************** DELETE /companies/:handle */
+
+describe("DELETE /jobs/:handle", function () {
+  test("works for users", async function () {
+    const resp = await request(app)
+        .delete(`/jobs/c1`)
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.body).toEqual({ deleted: "c1" });
+  });
+
+  test("unauth for anon", async function () {
+    const resp = await request(app)
+        .delete(`/jobs/c1`);
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test("not found for no such company", async function () {
+    const resp = await request(app)
+        .delete(`/jobs/nope`)
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(404);
+  });
+});
